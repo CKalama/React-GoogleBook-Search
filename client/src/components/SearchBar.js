a6e04a15477184e3cb76b8ff3516a91a95665a6b
Chris Kalama <chriskalama96@gmail.com>
2020-11-29 22:52:09 -0600

    tested functionality of onClick for button SearchBar, also added two pages: Home and Output as I assume I will need them

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -3,14 +3,17 @@ import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField"
 import Container from "@material-ui/core/Container"
 
+
+
 function SearchBar() {
+
     return (
         <Container className="search-bar-style" justifyContent="center" maxWidth="md">
             <TextField  id="outlined-basic" label="Search a Book" variant="outlined" />
 
             <br></br>
             <br></br>
-            <Button variant="contained" color="secondary">
+            <Button variant="contained" color="secondary" onClick={() => (console.log("button!"))}>
                 Search
             </Button>
             
