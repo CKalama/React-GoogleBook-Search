1873a38a69cc21aa2c0a46950feefd2b488bb75f
Chris Kalama <chriskalama96@gmail.com>
2020-11-30 01:03:10 -0600

    started building hooks to grab google API, leaving out key code for now dont want thievery.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+//
 import Header from "./components/Header"
 import SearchBar from "./components/SearchBar"
 
--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,20 +1,48 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField"
 import Container from "@material-ui/core/Container"
 
 
 
+
 function SearchBar() {
 
+    handleChange = (e) => {
+        //providing initial value of book as empty
+        //book is a string because thats what user types 
+        const [book, setBook] = useState("") 
+        //result is an array in the useState hook bc it will display items being called from Google API 
+        const [result, setResult] = useState([])
+        //Here is my APIKey from Google, where I will be compromised if searched publicly. 
+        const [apiKey, setApiKey] = useState("");
+    }
+
+    handleSubmit = (e) => {
+
+    }
+
     return (
-        <Container className="search-bar-style" justifyContent="center" maxWidth="md">
-            <TextField  id="outlined-basic" label="Search a Book" variant="outlined" />
+        <Container className="search-bar-style" maxWidth="md">
+            <TextField  
+            id="outlined-basic" 
+            label="Search a Book" 
+            variant="outlined"
+            onSubmit={handleSubmit} 
+            />
 
             <br></br>
             <br></br>
-            <Button variant="contained" color="secondary" onClick={() => (console.log("button!"))}>
-                Search
+
+            <Button 
+            variant="contained" 
+            color="secondary" 
+            onClick={() => (console.log("button!"))}
+            // onChange={onChange}
+
+            >
+            
+            Search
             </Button>
             
             
